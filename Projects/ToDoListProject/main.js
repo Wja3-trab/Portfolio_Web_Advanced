637e8c72fde4c7c2640396e35dacad8dea4dedd5
IbrahimBousba <hdydanb97@gmail.com>
2024-06-03 22:49:13 +0200

    todo list afgemaakt

--- Projects/ToDoListProject/main.js
+++ Projects/ToDoListProject/main.js
@@ -1,71 +1,99 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const taskForm = document.querySelector(".myForm");
-  const taskInput = document.getElementById("crntTask");
-  const taskLvl = document.getElementById("taskLevel");
-  const myList = document.querySelector(".myList");
-  const lvls = ["easy", "medium", "hard"];
-  const taskTemplate = (level, val, indx) => {
-    return `<li class='${level}'>
+import runMultipleFunctionsWithInitDelay from "./util.js";
+(function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const taskForm = document.querySelector(".myForm");
+    const taskInput = document.getElementById("crntTask");
+    const taskLvl = document.getElementById("taskLevel");
+    const myList = document.querySelector(".myList");
+    const lvls = ["easy", "medium", "hard"];
+
+    const taskTemplate = (level, val, indx) => {
+      return `<li id='${level}_${indx}' class='${level}'>
             <div class='item'>
                 <span>${val}</span>
                 <button id='delBtn' index='${indx}' key='${level}' class='${val}'>Del</button>
             </div>
         </li>`;
-  };
-  const checkLocalStorage = () => {
-    if (window.localStorage.getItem("tasks") == null) {
-      const temp = {
-        easy: [],
-        medium: [],
-        hard: [],
-      };
-      window.localStorage.setItem("tasks", JSON.stringify(temp));
-    }
-    return window.localStorage.getItem("tasks");
-  };
-  const tasksMap = JSON.parse(checkLocalStorage());
+    };
 
-  const updateList = (arr) => {
-    myList.innerHTML = "";
-    arr.map((key) => {
-      let x = 0;
-      tasksMap[key].map((elem) => {
-        myList.innerHTML += taskTemplate(key, elem, x);
-        x++;
+    const checkLocalStorage = () => {
+      if (window.localStorage.getItem("tasks") == null) {
+        const temp = {
+          easy: [],
+          medium: [],
+          hard: [],
+        };
+        window.localStorage.setItem("tasks", JSON.stringify(temp));
+      }
+      return JSON.parse(window.localStorage.getItem("tasks"));
+    };
+
+    const tasksMap = checkLocalStorage();
+
+    const destructureTasks = () => {
+      const { easy, medium, hard } = tasksMap;
+      const allTasksLvls = [...easy, ...medium, ...hard].filter(
+        (tmpArr) => tmpArr.length > 0
+      );
+      console.log(allTasksLvls);
+    };
+
+    const updateList = () => {
+      myList.innerHTML = "";
+      lvls.forEach((key) => {
+        tasksMap[key].map((elem, x) => {
+          myList.innerHTML += taskTemplate(key, elem, x);
+        });
       });
-    });
-  };
-  const delTask = (e) => {
-    const count = e.target;
-    console.log(count.getAttribute("key"));
-    tasksMap[count.getAttribute("key")].splice(count.getAttribute("index"), 1);
-    window.localStorage.setItem("tasks", JSON.stringify(tasksMap));
+    };
+
+    const delTask = (e) => {
+      const elm = e.target;
+      const liElem = document.getElementById(
+        elm.getAttribute("key") + "_" + elm.getAttribute("index")
+      );
+      const divToDelete = liElem.querySelector(".item");
+      console.log(elm.getAttribute("key"));
+      tasksMap[elm.getAttribute("key")].splice(elm.getAttribute("index"), 1);
+      window.localStorage.setItem("tasks", JSON.stringify(tasksMap));
+      liElem.classList.add("delete");
+      divToDelete.classList.add("delete");
+      runMultipleFunctionsWithInitDelay(
+        1500,
+        updateList,
+        addListenerToDelBtn,
+        destructureTasks
+      );
+    };
+
+    const addListenerToDelBtn = () => {
+      const delBtns = document.querySelectorAll("#delBtn");
+      for (let item of delBtns) {
+        item.addEventListener("click", delTask);
+      }
+    };
+
+    const submitTask = (e) => {
+      e.preventDefault();
+      const lvl = taskLvl.value;
+      const tskInp = taskInput.value;
+
+      if (tskInp.trim() != "") {
+        console.log(tskInp + "\n" + lvl);
+        let myArr = [...tasksMap[lvl], tskInp].filter(
+          (item) => item.trim().length > 9
+        );
+        taskInput.value = "";
+        tasksMap[lvl] = myArr;
+        window.localStorage.setItem("tasks", JSON.stringify(tasksMap));
+        updateList(lvls);
+        addListenerToDelBtn();
+        destructureTasks();
+      }
+    };
+
     updateList(lvls);
     addListenerToDelBtn();
-  };
-  const addListenerToDelBtn = () => {
-    const delBtns = document.querySelectorAll("#delBtn");
-    for (let item of delBtns) {
-      item.addEventListener("click", delTask);
-    }
-  };
-  const submitTask = (e) => {
-    e.preventDefault();
-    const lvl = taskLvl.value;
-    const tskInp = taskInput.value;
-    console.log(tskInp + "\n" + lvl);
-    let myArr = [];
-    myArr.push(...tasksMap[lvl]);
-    //console.log(myArr);
-    myArr.push(tskInp);
-    //console.log(myArr);
-    taskInput.value = "";
-    tasksMap[lvl] = myArr;
-    window.localStorage.setItem("tasks", JSON.stringify(tasksMap));
-    updateList(lvls);
-    addListenerToDelBtn();
-  };
-  updateList(lvls);
-  addListenerToDelBtn();
-  taskForm.addEventListener("submit", submitTask);
-});
+    taskForm.addEventListener("submit", submitTask);
+  });
+})();
